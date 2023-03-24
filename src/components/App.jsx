4914cff8ffdda230b31e3026b6328d5913b58d37
Zbigniew-Klimczak <111083175+Zbigniew-Klimczak@users.noreply.github.com>
2023-03-24 01:05:41 +0100

    Tough, I must say

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,99 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+export class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      contacts: [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ],
+      name: '',
+      number: '',
+      filter: '',
+    };
+  }
+  handleChange = evt => {
+    const { name } = evt.currentTarget;
+    this.setState({ [name]: evt.currentTarget.value });
+    if (name === 'filter') {
+      this.handleFilter();
+    }
+  };
+  handleSubmit = evt => {
+    evt.preventDefault();
+    const { name, number } = this.state;
+    const form = evt.currentTarget;
+    const id = nanoid();
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, { name: name, number: number, id: id }],
+    }));
+    form.reset();
+  };
+  handleFilter = () => {
+    const { contacts, filter } = this.state;
+    const keyword = filter.toLowerCase();
+    const filteredContacts = contacts.filter(
+      contact => contact.name.toLowerCase().indexOf(keyword) > -1
+    );
+    console.log(filteredContacts);
+  };
+  render() {
+    const { name, contacts, number, filter } = this.state;
+    return (
+      <>
+        <h2>Phonebook</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter name"
+              value={name}
+              onChange={this.handleChange}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <label>
+            Number
+            <input
+              type="tel"
+              name="number"
+              placeholder="Enter number"
+              value={number}
+              onChange={this.handleChange}
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+        <h2>Contacts</h2>
+        <label>
+          Find contacts by name
+          <input
+            type="text"
+            name="filter"
+            placeholder="Enter name"
+            value={filter}
+            onChange={this.handleChange}
+          />
+        </label>
+        <ul>
+          {contacts.map(contact => (
+            <li key={contact.id}>
+              {contact.name}: {contact.number}
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
